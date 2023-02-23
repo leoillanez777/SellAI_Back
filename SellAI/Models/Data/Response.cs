70cc8eb87e3735efd4dc5da0fc209cebd7a85671
leonardoillanez <Leonardo@live.com.ar>
2023-02-23 00:26:24 -0300

    Muchos cambios 😵‍💫

---
+++ SellAI/Models/Data/Response.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SellAI.Models.Data
+{
+  public class ResponseError
+  {
+    public string error { get; set; } = null!;
+    public string code { get; set; } = null!;
+  }
+}
+
