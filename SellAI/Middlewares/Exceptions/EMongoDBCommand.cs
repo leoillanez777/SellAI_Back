60493fe0e941f7ec0d5f7cbab6fc582d736dd300
leonardoillanez <Leonardo@live.com.ar>
2023-03-09 22:00:46 -0300

    Se agrega la lectura de los datos.

---
+++ SellAI/Middlewares/Exceptions/EMongoDBCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SellAI.Middlewares.Exceptions;
+
+public class EMongoDBQuery : Exception {
+  public EMongoDBQuery() { }
+
+  public EMongoDBQuery(string message) : base(message) { }
+
+  public EMongoDBQuery(string message, Exception innerException) : base(message, innerException) { }
+}
+
