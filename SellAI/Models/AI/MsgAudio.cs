f4e899fde6ff0b1188927f77f1728b4c9d649796
leonardoillanez <Leonardo@live.com.ar>
2023-03-13 23:19:12 -0300

    Add audio message

---
+++ SellAI/Models/AI/MsgAudio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+using SellAI.Models.AI.Objects;
+
+namespace SellAI.Models.AI
+{
+  public class Audio
+  {
+    [JsonProperty("text")]
+    public string Text { get; set; } = null!;
+
+    [JsonProperty("intents")]
+    public List<Intents>? Intents { get; set; }
+
+    [JsonProperty("entities")]
+    public IDictionary<string, List<Entities>>? Entities { get; set; }
+
+    [JsonProperty("is_final")]
+    public bool? EsFinal { get; set; }
+  }
+}
+
